b5aadd401e1d7ba32f7b69d7f53828dc3df01892
Gourika Khandelwal <gourika222772@gmail.com>
2026-08-23 15:13:20 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0;
+        int h=nums.size()-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]>target){
+                h=mid-1;
+            }else if(nums[mid]==target){
+                return mid;
+            }else{
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
+};
