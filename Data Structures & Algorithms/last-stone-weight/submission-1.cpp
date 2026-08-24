becb4ab81c15992b2253454f02b7cfe0f4e66f8b
Gourika Khandelwal <gourika222772@gmail.com>
2026-08-25 01:49:54 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int num:stones){
+            pq.push(num);
+        }
+        while(pq.size()>1){
+            int first=pq.top();
+            pq.pop();
+            int second=pq.top();
+            pq.pop();
+            if(first==second){
+                continue;
+            }else{
+                pq.push(first-second);
+            }
+        }
+        if(pq.empty()){
+            return 0;
+        }
+        return pq.top();
+    }
+};
