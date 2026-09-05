983f3c7399813093f7ce9c089121a8d8cd8d90ea
Gourika Khandelwal <gourika222772@gmail.com>
2026-09-05 23:15:26 +0530

    Add: burst-balloons - submission-0

---
+++ Data Structures & Algorithms/burst-balloons/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+int func(vector<int>& nums,int i,int j){
+    if(i>j)return 0;
+    int maxcoins=INT_MIN;
+    for(int k=i;k<=j;k++){
+        int coins=nums[i-1]*nums[k]*nums[j+1];
+        coins+=func(nums,i,k-1)+func(nums,k+1,j);
+        maxcoins=max(maxcoins,coins);
+    }
+    return maxcoins;
+}
+    int maxCoins(vector<int>& nums) {
+        int n=nums.size();
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+         for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n; j++) {
+            if (i > j) continue;
+            int maxi = INT_MIN;
+            
+          
+            for (int ind = i; ind <= j; ind++) {
+             
+                int coins = nums[i - 1] * nums[ind] * nums[j + 1];
+              
+                int remainingCoins = dp[i][ind - 1] + dp[ind + 1][j];
+                
+      
+                maxi = max(maxi, coins + remainingCoins);
+            }
+            
+            dp[i][j] = maxi;
+        }
+    }
+    
+    return dp[1][n];
+    }
+};
